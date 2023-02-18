93d3426c467f72f03d3e4aba079790a6eea9cde3
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-02-18 22:51:49 +0100

    did utils

--- user/xargs.c
+++ user/xargs.c
@@ -1,6 +1,7 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/param.h"
 
 int
 main(int argc, char* argv[]) {
@@ -10,45 +11,37 @@ main(int argc, char* argv[]) {
   }
 
   char* command = argv[1];
-  if (fork() == 0) {
-    printf("child running %s\n", command);
-    exec(command, argv + 1);
-  } else wait(0);
+
   const int maxLength = 512;
   char line[maxLength];
-  const int stdin = 0;
-  int offset = 0;
-  char c = 0;
-  char* myArgs[3];
-  myArgs[0] = command;
-  myArgs[1] = line;
-  myArgs[2] = 0;
-  // printf("while loop about to run\n");
-  while (read(stdin, &c, 1)) {
-    // printf("%c", c);
-    if (c == '\n') {
-      line[offset] = 0;
-      if (fork() == 0) {
-        // printf("loop child about to run %s %s", myArgs[0], myArgs[1]);
-        exec(command, myArgs);
-        // exec(command, argv);
-      } else {
-        wait(0);
-        offset = 0;
-      }
-    } else {
-      line[offset++] = c;
-    }
-  } if (offset != 0) {
-    line[offset] = 0;
-    if (fork() == 0) {
-      line[offset] = 0;
-      exec(command, myArgs);
-    } else {
-      wait(0);
-      offset = 0;
-    }
+
+  int myArgc = 1;
+  char* myArgv[MAXARG - 1];
+  for (int i = 0; i < MAXARG - 1; ++i) {
+    myArgv[i] = malloc(128);
+    myArgv[i][0] = 0;
   }
 
-  exit(0);
+  for (; myArgc < argc; ++myArgc) {
+    strcpy(myArgv[myArgc - 1], argv[myArgc]);
+  }
+
+  const int stdin = 0;
+  int length = 0;
+  char c = 0;
+
+  while (read(stdin, &c, 1) && myArgc < MAXARG - 1) {
+    if (c == '\n') {
+      line[length] = 0;
+      strcpy(myArgv[myArgc++], line);
+      length = 0;
+    } else {
+      line[length++] = c;
+    }
+  } if (length != 0) {
+    line[length] = 0;
+    strcpy(myArgv[myArgc++], line);
+  }
+  printf("%d", exec(command, myArgv));
+  exit(-1);
 }
